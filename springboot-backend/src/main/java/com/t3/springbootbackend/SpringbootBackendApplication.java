81b404e709197bc9a4b335203d4ac41ffd12f99e
asaph-t-Souza <asaph.net@gmail.com>
2023-05-25 20:13:21 -0300

    a

--- springboot-backend/src/main/java/com/t3/springbootbackend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/t3/springbootbackend/SpringbootBackendApplication.java
@@ -1,7 +1,14 @@
 package com.t3.springbootbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.t3.springbootbackend.entity.User;
+import com.t3.springbootbackend.repository.LocationRepository;
+import com.t3.springbootbackend.repository.ReviewRepository;
+import com.t3.springbootbackend.repository.UserRepository;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
@@ -9,5 +16,17 @@ public class SpringbootBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository userRepository, LocationRepository locationRepository, ReviewRepository reviewRepository){
+		return args -> {
+			System.out.println("INICIO");
+
+			User user = new User();
+
+			System.out.println("FIM");
+		};
+	}
+
 
 }
